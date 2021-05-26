0abcaef9e0bd6fea386053dffe0e7bac1b15688c
pija2021 <trinhpeter839@gmail.com>
2021-05-26 01:23:47 +0000

    adding all files to github

---
+++ prototype/login.js
@@ -0,0 +1,15 @@
+function login(){
+    window.location = "home_page.html";
+}
+
+function login_first(){
+    window.location = "login.html";
+}
+
+function register(){
+    window.location = "register.html";
+}
+
+function check_in(){
+    window.location = "check_in.html";
+}
