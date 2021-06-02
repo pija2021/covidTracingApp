04f31c82b6a82bbf90b2d48714c88962342d9c75
a1773884 <a1773884@student.adelaide.edu.au>
2021-06-02 21:53:03 +0930

    moving in html files and javascript, css into server folder

---
+++ project/public/javascript/login.js
@@ -0,0 +1,29 @@
+var username="root"
+var password="toor"
+
+function login(){
+    usr_name = document.getElementById("login_id").value
+    pass = document.getElementById("login_password").value
+
+    if (usr_name==username && pass==password)
+    {
+        window.location = "home_page.html";
+    }
+    else
+    {
+        alert("Incorrect login details, please try again.");
+    }
+}
+
+//whats this?
+function login_first(){
+    window.location = "login.html";
+}
+
+function register(){
+    window.location = "register.html";
+}
+
+function check_in(){
+    window.location = "check_in.html";
+}
