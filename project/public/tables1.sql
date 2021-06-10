a46f747dd8404bb4f7150cb52f5018e6a5fbabcb
a1784585 <a1784585@student.adelaide.edu.au>
2021-06-10 17:18:23 +0000

    login initial

---
+++ project/public/tables1.sql
@@ -0,0 +1,251 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: tables
+-- ------------------------------------------------------
+-- Server version	8.0.19-0ubuntu5
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `tables`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `tables` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `tables`;
+
+--
+-- Table structure for table `AddressLine`
+--
+
+DROP TABLE IF EXISTS `AddressLine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `AddressLine` (
+  `id` int NOT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `AddressLine`
+--
+
+LOCK TABLES `AddressLine` WRITE;
+/*!40000 ALTER TABLE `AddressLine` DISABLE KEYS */;
+INSERT INTO `AddressLine` VALUES (1,'123 Fake St');
+/*!40000 ALTER TABLE `AddressLine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Customer`
+--
+
+DROP TABLE IF EXISTS `Customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Customer` (
+  `id` int NOT NULL,
+  `First_Name` varchar(50) DEFAULT NULL,
+  `Last_Name` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  `Contact` int NOT NULL,
+  `History` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customer`
+--
+
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+INSERT INTO `Customer` VALUES (1,'John','Smith','johnsmith1@gmail.com','22 Rose St Rosewater',412345678,NULL);
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `History`
+--
+
+DROP TABLE IF EXISTS `History`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `History` (
+  `id` int NOT NULL,
+  `Venue` varchar(50) DEFAULT NULL,
+  `Date_Time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `History`
+--
+
+LOCK TABLES `History` WRITE;
+/*!40000 ALTER TABLE `History` DISABLE KEYS */;
+/*!40000 ALTER TABLE `History` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Hotspot`
+--
+
+DROP TABLE IF EXISTS `Hotspot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Hotspot` (
+  `id` int NOT NULL,
+  `Official` int DEFAULT NULL,
+  `Venue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Hotspot`
+--
+
+LOCK TABLES `Hotspot` WRITE;
+/*!40000 ALTER TABLE `Hotspot` DISABLE KEYS */;
+INSERT INTO `Hotspot` VALUES (1,NULL,'Subway');
+/*!40000 ALTER TABLE `Hotspot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Managers`
+--
+
+DROP TABLE IF EXISTS `Managers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Managers` (
+  `id` int NOT NULL,
+  `First_Name` varchar(50) DEFAULT NULL,
+  `Last_Name` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  `Contact` int NOT NULL,
+  `History` int DEFAULT NULL,
+  `Venue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Managers`
+--
+
+LOCK TABLES `Managers` WRITE;
+/*!40000 ALTER TABLE `Managers` DISABLE KEYS */;
+INSERT INTO `Managers` VALUES (1,'Andy','Lam','Andylam3@gmail.com','18 Eastern Parade Ottoway',411223344,NULL,NULL);
+/*!40000 ALTER TABLE `Managers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Official`
+--
+
+DROP TABLE IF EXISTS `Official`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Official` (
+  `id` int NOT NULL,
+  `First_Name` varchar(50) DEFAULT NULL,
+  `Last_Name` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  `Contact` int NOT NULL,
+  `History` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Official`
+--
+
+LOCK TABLES `Official` WRITE;
+/*!40000 ALTER TABLE `Official` DISABLE KEYS */;
+INSERT INTO `Official` VALUES (1,'Ella','Musk','Ellamusk2@gmail.com','23 Rose St Rosewater',498765432,NULL);
+/*!40000 ALTER TABLE `Official` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Venue`
+--
+
+DROP TABLE IF EXISTS `Venue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Venue` (
+  `id` int NOT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  `Date_Time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Venue`
+--
+
+LOCK TABLES `Venue` WRITE;
+/*!40000 ALTER TABLE `Venue` DISABLE KEYS */;
+INSERT INTO `Venue` VALUES (1,'GongCha','40 Hindley St',NULL);
+/*!40000 ALTER TABLE `Venue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'test','test','test@test.com');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-10 17:17:15
