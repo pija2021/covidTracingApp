bd7e1a67b1f8c8e62d3a8782f5034e137d9a4875
pija2021 <trinhpeter839@gmail.com>
2021-06-14 13:10:47 +0000

    public/javascript/login.js restore

---
+++ project/public/javascript/login.js
@@ -0,0 +1,16 @@
+
+}
+
+
+//whats this?
+function login_first(){
+    window.location = "login.html";
+}
+
+function register(){
+    window.location = "register.html";
+}
+
+function check_in(){
+    window.location = "check_in.html";
+}
